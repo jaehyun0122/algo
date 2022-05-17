537cb5a4af1f337e2df2209d25996c51704053fb
재현 <jungjh122@naver.com>
2022-05-17 23:36:46 +0900

    bj 숨바꼭질 12851

---
+++ 220515/src/숨바꼭질12851.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class 숨바꼭질12851 {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k =Integer.parseInt(st.nextToken());
+	}
+
+}
