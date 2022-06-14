db595d08b4461295fd865fa432927c64e210d6c7
jaehyun <jungjh122@naver.com>
2022-06-15 00:32:13 +0900

    bj 1244 새로운 풀이

--- 220614/src/스위치켜고끄기1244.java
+++ 220614/src/스위치켜고끄기1244.java
@@ -7,7 +7,6 @@ public class 스위치켜고끄기1244 {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		int[] arr = new int[T];
-		//스위치 배열에 값 넣음
 		for(int i=0; i<T; i++) {
 			arr[i] = sc.nextInt();
 		}
@@ -17,25 +16,31 @@ public class 스위치켜고끄기1244 {
 		for(int i=0; i<s_num; i++) {
 			gen = sc.nextInt();
 			num = sc.nextInt();
-			//남자
 			if(gen == 1) {
-				for(int j=1; j<=T/num; j++) {
-					arr[(num*j)-1] = arr[(num*j)-1] ==1? 0:1;
+				int cnt = 1;
+				while(true) {
+					if(num*cnt-1 >= T) break;
+					arr[num*cnt-1] = arr[num*cnt-1] == 1? 0: 1;
+					cnt++;
 				}
 			}
-			//여자
 			else {
-				arr[num-1] = arr[num-1] == 1? 0: 1;
-				for(int j=1; j<num; j++) {
-					if(arr[num-j-1] != arr[num+j-1] || num-j-1 < 0 || num+j-1 > T-1) break;
-					
-					arr[num-j-1]= arr[num+j-1] = arr[num-j-1] == 1? 0:1;
+				int j = 1;
+				arr[num-1] = arr[num-1] == 1 ? 0 : 1;
+				while(true) {
+					if(num-1-j < 0 || num-1+j>=T) {
+						break;
+					}
+					if(arr[num-1-j] == arr[num-1+j]) {
+						arr[num-1-j] = arr[num-1+j] = arr[num-1+j] == 1 ? 0 : 1;
+						j++;
+					}else break;
 				}
 			}
 		}
-		for(int i = 1; i < arr.length; i++) {
+		for(int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
-			if(i % 20 == 0)
+			if((i+1) % 20 == 0)
 				System.out.println();
 		}
 		
